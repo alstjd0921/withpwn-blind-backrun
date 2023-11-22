4bc5e905e3146cd6a1636100aa8f753a1bd43ee1
alstjd0921 <kwonarseus@gmail.com>
2023-11-22 19:29:11 +0900

    first commit

---
+++ src/main.rs
@@ -0,0 +1,58 @@
+use std::sync::Arc;
+
+use anyhow::Result;
+use artemis_core::{
+    collectors::mevshare_collector::MevShareCollector,
+    engine::Engine,
+    executors::mev_share_executor::MevshareExecutor,
+    types::{CollectorMap, ExecutorMap},
+};
+use clap::Parser;
+use ethers::{
+    prelude::MiddlewareBuilder,
+    providers::{Provider, Ws},
+    signers::{LocalWallet, Signer},
+    types::Address,
+};
+use tracing::{info, Level};
+use tracing_subscriber::{filter, prelude::*};
+
+/// CLI Options.
+#[derive(Parser, Debug)]
+pub struct Args {
+    /// Ethereum node WS endpoint.
+    #[arg(long)]
+    pub wss: String,
+    /// Private key for sending txs.
+    #[arg(long)]
+    pub private_key: String,
+    /// MEV share signer
+    #[arg(long)]
+    pub flashbots_signer: String,
+    /// Address of the arb contract.
+    #[arg(long)]
+    pub arb_contract_address: Address,
+}
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    // Set up tracing and parse args.
+    let filter = filter::Targets::new().with_target("artemis_core", Level::INFO);
+    tracing_subscriber::registry()
+        .with(tracing_subscriber::fmt::layer())
+        .with(filter)
+        .init();
+
+    let args = Args::parse();
+
+    let ws = Ws::connect(args.wss).await?;
+    let provider = Provider::new(ws);
+
+    let wallet: LocalWallet = args.private_key.parse().unwrap();
+    let address = wallet.address();
+
+    let provider = Arc::new(provider.nonce_manager(address).with_signer(wallet.clone()));
+    let fb_signer: LocalWallet = args.flashbots_signer.parse().unwrap();
+
+    Ok(())
+}
